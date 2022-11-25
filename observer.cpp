ffb90d96aaf7a3cd343df884a81131c5689196f5
Kris <Valentina10@sibmail.com>
2022-11-25 23:17:48 +0700

    Check for existence and size.

--- observer.cpp
+++ observer.cpp
@@ -9,4 +9,24 @@ void FileObserver::update(bool exist, bool new_exist, int size, int new_size) //
   //new_exist - существует ли файл сейчас/
   //size - размер файла который был при прошлой проверка
   //new_size - размер файла новый
+
+    if (new_exist != exist) //если существование файла изменилось
+    {
+        if (new_exist) //если сущетсвует сейчас
+        {
+            printf("The file exists, the file is not empty. Size = %d bytes\n", new_size);
+        }
+        else //если файл сейчас не существует
+        {
+            printf("File does not exist\n");
+        }
+    }
+    else //если состояние файла не изменилось
+    {
+        if (new_exist && new_size != size) //проверяем не изменился ли размер
+        {
+            printf("The file exists, the file has been modified. New size = %d bytes\n", new_size);
+        }
+    }
 }
+
