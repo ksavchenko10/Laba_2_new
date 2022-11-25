2b114f33362cba845a383edacca9daa0621dd2ba
Kris <Valentina10@sibmail.com>
2022-11-25 14:21:13 +0700

    Creating an FSubject class to work with a file.

--- observer.h
+++ observer.h
@@ -1,6 +1,9 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
+#include <Qstring>
+#include <vector>
+
 class Observer //абстрактный класс родитель наблюдателей
 {
  public:
@@ -14,5 +17,14 @@ public:
         void update(bool exist, bool new_exist, int size, int new_size); //переопределяем виртуальный метод родителя
 };
 
+class FSubject //класс для работы с файлом
+{
+    std::vector<Observer*> list; //вектор содержащий наблюдателей
+
+    public:
+    QString file_path; //путь к файлу
+    bool file_exist = false; //существование файла
+    int file_size = 0; //размер файла
+};
 
 #endif // OBSERVER_H
