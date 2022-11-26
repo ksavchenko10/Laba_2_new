dc6fe9e2131661122a2e8273e03a151d9605c694
Kris <Valentina10@sibmail.com>
2022-11-26 20:44:14 +0700

    Add to int main().

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <QCoreApplication>
 
+#include <stdio.h>
+#include <QFile>
+#include <iostream>
+
+#include "observer.h"
+#include "observer.cpp"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    FileObserver obs; //создаем экземпляр наблюдателя
+    FSubject file("test.txt"); //создаем экзепляр состояния файла
+
+    file.attach(&obs); //добавляем наблюдателя к экземпляру состояния файла
+
+    file.startNotify(); //запускаем постоянную проверку состояния файла
 
     return a.exec();
 }
